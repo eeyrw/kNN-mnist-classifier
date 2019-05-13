11cd1643f52daac2eb6031ffe09ef6cd0b88fa35
yuan <yrw@sdec.com.cn>
2019-05-13 16:10:24 +0800

    M

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 #include "MnistLoader.h"
 #include "kNN.h"
 
+
+double Evaluate(MNIST_CHAR_LIST mnistTrainList,MNIST_CHAR_LIST mnistTestList,int kNb)
+{
+    uint32_t errorCount=0;
+    printf("Start evaluating kNN with k=%d.\n",kNb);
+    for(uint32_t i=0;i<mnistTestList.len;i+=1)
+    {
+        uint8_t result=KnnFit(&mnistTrainList,&mnistTestList.chars[i],kNb);
+        if(result!=mnistTestList.chars[i].label)
+            errorCount++;
+
+        if((i+1)%1000==0)
+        {
+            printf("Tested: %d Accuracy:%f\n",i+1,1-(double)errorCount/(i+1));
+        }
+    }
+    printf("Test finished. Final accuracy: %f",1-(double)errorCount/mnistTestList.len);
+    return (1-(double)errorCount/mnistTestList.len);
+}
+
 int main(void)
 {
     MNIST_CHAR_LIST mnistTrainList;
@@ -10,19 +30,13 @@ int main(void)
     mnistTestList=MnistLoad("./dataSet/t10k-images.idx3-ubyte","./dataSet/t10k-labels.idx1-ubyte");
 
 
-    uint32_t errorCount=0;
-    for(uint32_t i=0;i<mnistTestList.len;i+=1)
-    {
-        uint8_t result=KnnFit(&mnistTrainList,&mnistTestList.chars[i],7);
-        if(result!=mnistTestList.chars[i].label)
-            errorCount++;
+    double acc_k1=Evaluate(mnistTrainList,mnistTestList,1);
+    double acc_k3=Evaluate(mnistTrainList,mnistTestList,3);
+    double acc_k5=Evaluate(mnistTrainList,mnistTestList,5);
+    double acc_k9=Evaluate(mnistTrainList,mnistTestList,9);
 
-        if((i+1)%100==0)
-        {
-            printf("Tested: %d Accuracy:%f\n",i+1,1-(double)errorCount/(i+1));
-        }
-    }
-    printf("Test finished. Final accuracy: %f",1-(double)errorCount/mnistTestList.len);
+    printf("Acc K=1: %f, Acc K=3: %f, Acc K=5: %f, Acc K=9: %f\n",acc_k1,acc_k3,acc_k5,acc_k9);
+
     getchar();
 
     return 0;
