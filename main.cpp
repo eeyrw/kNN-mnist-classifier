38de899fa5b5ecacac5694b581a91827f18eb419
yuan <ayrw@163.com>
2019-10-29 19:47:09 +0800

    M

--- main.cpp
+++ main.cpp
@@ -5,33 +5,13 @@
 #include "jpeg_decoder.h"
 
 
-double Evaluate(MNIST_CHAR_LIST mnistTrainList,MNIST_CHAR_LIST mnistTestList,int kNb)
-{
-    uint32_t errorCount=0;
-    printf("Start evaluating kNN with k=%d.\n",kNb);
-    for(uint32_t i=0;i<mnistTestList.len;i+=1)
-    {
-        uint8_t result=KnnFit(&mnistTrainList,&mnistTestList.chars[i],kNb);
-        if(result!=mnistTestList.chars[i].label)
-            errorCount++;
-
-        if((i+1)%1000==0)
-        {
-            printf("Tested: %d Accuracy:%f\n",i+1,1-(double)errorCount/(i+1));
-        }
-    }
-    printf("Test finished. Final accuracy: %f",1-(double)errorCount/mnistTestList.len);
-    return (1-(double)errorCount/mnistTestList.len);
-}
-
 int recognize(MNIST_CHAR_LIST mnistTrainList,MNIST_CHAR &mnistChar)
 {
 
-        uint8_t result=KnnFit(&mnistTrainList,&mnistChar,3);
-        return static_cast<int>(result);
+    uint8_t result=KnnFit(&mnistTrainList,&mnistChar,3);
+    return static_cast<int>(result);
 
 }
-
 
 int readImageToMnistChar(char* path,MNIST_CHAR &mnistChar)
 {
@@ -40,7 +20,8 @@ int readImageToMnistChar(char* path,MNIST_CHAR &mnistChar)
     FILE *f;
 
     f = fopen(path, "rb");
-    if (!f) {
+    if (!f)
+    {
         printf("Error opening the input file.\n");
         return 1;
     }
@@ -59,14 +40,12 @@ int readImageToMnistChar(char* path,MNIST_CHAR &mnistChar)
     }
 
     printf("IsColor:%d W:%d H:%d\n", decoder.IsColor() ? 1 : 0, decoder.GetWidth(), decoder.GetHeight());
-    //fwrite(decoder.GetImage(), 1, decoder.GetImageSize(), f);
     if(decoder.IsColor())
     {
         unsigned char* image=decoder.GetImage();
         int j=0;
-        for(int i=0;i<decoder.GetImageSize();i+=3)
+        for(int i=0; i<decoder.GetImageSize(); i+=3)
         {
-
             mnistChar.pixels[j]=image[i];
             j++;
         }
@@ -84,29 +63,17 @@ int main(void)
     mnistTestList=MnistLoad("./dataSet/t10k-images.idx3-ubyte","./dataSet/t10k-labels.idx1-ubyte");
 
 
-//    double acc_k1=Evaluate(mnistTrainList,mnistTestList,1);
-//    double acc_k3=Evaluate(mnistTrainList,mnistTestList,3);
-//    double acc_k5=Evaluate(mnistTrainList,mnistTestList,5);
-//    double acc_k9=Evaluate(mnistTrainList,mnistTestList,9);
-//
-//    printf("Acc K=1: %f, Acc K=3: %f, Acc K=5: %f, Acc K=9: %f\n",acc_k1,acc_k3,acc_k5,acc_k9);
-
-
-
     MNIST_CHAR mnistChar;
-    char path[1024];
+    char path[2048];
     while(true)
     {
         printf("Please input your handwriting image path:\n");
         gets(path);
-      readImageToMnistChar(path,mnistChar);
-    auto result = recognize(mnistTrainList,mnistChar);
-    printf("Your number is: %d\n",result);
+        readImageToMnistChar(path,mnistChar);
+        auto result = recognize(mnistTrainList,mnistChar);
+        printf("Your number is: %d\n",result);
 
     }
-
-
-    getchar();
 
     return 0;
 }
